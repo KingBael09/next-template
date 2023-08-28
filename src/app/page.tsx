350f3b03e43ef7033f2b2bfc9ba977e04aeecca6
KingBael <www.jayesh567@gmail.com>
2023-08-29 02:34:54 +0530

    feat: updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,3 @@
 export default function Home() {
-  return (
-    <main className="container">
-      <h1>Hello World</h1>
-    </main>
-  )
+  return <main className="container">Hello World</main>
 }
